58d91adfb432d5c43efb3f4f6a18b7810745cc39
Diyal Behzad Mirza <diyalmirza_eight@hotmail.com>
2026-06-11 16:16:04 +0500

    Initial commit

---
+++ dummy_frontend.py
@@ -0,0 +1,112 @@
+# Step5: Streamlit dashboard (Just for testing)
+
+import streamlit as st
+import datetime as dt
+import requests
+
+
+st.title("Agha Khan Appointment Booking Portal")
+base_url= st.text_input("Backend URL", "http://localhost:4444").rstrip("/")
+
+# Inputs
+patient_name = st.text_input("Patient name")
+reason = st.text_input("Reason")
+start_date = st.date_input("Date", value=dt.date.today() + dt.timedelta(days=1))
+start_time = st.time_input("Time", value=dt.time(9, 0))
+
+# Button
+if st.button("Schedule"):
+    start_dt = dt.datetime.combine(start_date, start_time)
+
+    payload = {
+        "patient_name": patient_name.strip(),
+        "reason": reason.strip() or None,
+        "start_time": start_dt.isoformat(),
+    }
+
+    try:
+        resp = requests.post(
+            f"{base_url}/schedule_appointment/",
+            json=payload,
+            timeout=10
+        )
+        resp.raise_for_status()
+        st.success("Scheduled successfully ✅")
+
+    except requests.RequestException as exc:
+        st.error(f"Schedule failed: {exc}")
+
+
+
+st.divider()
+st.subheader("Cancel")
+
+
+# Inputs
+cancel_name = st.text_input("Patient name to cancel", key="cancel_name")
+cancel_date = st.date_input("Date to cancel", key="cancel_date", value=dt.date.today())
+
+# Button
+if st.button("Cancel appointment"):
+
+    payload = {
+        "patient_name": cancel_name.strip(),
+        "date": cancel_date.isoformat()
+    }
+
+    try:
+        resp = requests.post(f"{base_url}/cancel_appointment/", json=payload, timeout=10)
+
+        resp.raise_for_status()
+        data = resp.json() if resp.content else {}
+        st.success(f"Canceled: {data.get('canceled_count', 0)} appointments")
+   
+
+    except requests.HTTPError:
+        st.error(resp.text)
+
+    except requests.RequestException as exc:
+        st.error(f"Cancel failed: {exc}")
+
+
+
+
+
+
+appointment_date = st.date_input(
+    "Date to check appointments",
+    key="check_appointment_date",
+    value=dt.date.today()
+)
+
+if st.button("Check appointments"):
+
+    params = {
+        "date": appointment_date.isoformat()
+    }
+
+    try:
+        # resp = requests.get(
+        #     f"{base_url}/list_appointments/",
+        #     params=params,
+        #     timeout=10
+        # )
+
+        resp = requests.post(
+             f"{base_url}/list_appointments/",
+             json=params,
+             timeout=10
+          )
+
+        resp.raise_for_status()
+
+        data = resp.json()
+
+        st.dataframe(
+            data,
+            use_container_width=True,
+            hide_index=True
+        )
+
+    except requests.RequestException as exc:
+        st.error(f"Could not load appointments: {exc}")
